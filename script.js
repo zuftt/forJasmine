d11a0e5606e375865111edb3d67c756066fa43fc
zuftt <muhdzafri2015@gmail.com>
2025-02-07 00:08:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function displayCat() {
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.src = 'meme.jpg'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
